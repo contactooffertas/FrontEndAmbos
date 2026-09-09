a1b16d89504bd101094b8185ad252cd5821ff834
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 18:21:40 -0300

    fix: refrescar identidad vendedor antes del navbar

--- app/componentes/MainLayout.tsx
+++ app/componentes/MainLayout.tsx
@@ -2,10 +2,12 @@
 import Navbar from './navbar';
 import Footer from './footer';
 import SellerOnboardingBanner from './SellerOnboardingBanner';
+import SellerSessionSync from './SellerSessionSync';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <SellerSessionSync />
       <Navbar />
       <main>
         <SellerOnboardingBanner />
