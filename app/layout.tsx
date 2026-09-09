03ba3b988980a9278229141317577d40127b6ad5
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 17:02:24 -0300

    fix: evitar capas CSS duplicadas en afiliados

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Providers } from '../app/context/Providers';
 import PWAManifestUpdater from './componentes/PWAManifestUpdater';
 import './globals.css';
 import './styles/product-card-polish.css';
-import './styles/affiliate-flow-polish.css';
 import './styles/affiliate-rosario-brand.css';
 
 const SITE = 'https://www.rosariomarket.com.ar';
