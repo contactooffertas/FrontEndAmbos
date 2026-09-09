0e8e5204ad69ab37dd71023a8f3cb17fc7a53532
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 18:00:12 -0300

    feat: mostrar acceso claro para crear negocio

--- app/componentes/MainLayout.tsx
+++ app/componentes/MainLayout.tsx
@@ -1,12 +1,16 @@
 // components/MainLayout.tsx
 import Navbar from './navbar';
 import Footer from './footer';
+import SellerOnboardingBanner from './SellerOnboardingBanner';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main>
+        <SellerOnboardingBanner />
+        {children}
+      </main>
       <Footer />
     </>
   );
