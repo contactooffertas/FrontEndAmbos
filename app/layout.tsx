871911c5a3f14c897e1a72560316a329345345ef
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 13:59:07 -0300

    style: activar pulido de tarjetas de producto

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from '../app/context/Providers';
 import PWAManifestUpdater from './componentes/PWAManifestUpdater';
 import './globals.css';
 import './styles/home-polish.css';
+import './styles/product-card-polish.css';
 
 const SITE = 'https://www.rosariomarket.com.ar';
 
