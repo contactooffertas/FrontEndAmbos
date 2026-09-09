5a354690038d6c6014d74b7136bc0e38f2dba873
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 18:27:51 -0300

    seo: corregir rutas privadas en robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,25 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
+
+const SITE = "https://www.rosariomarket.com.ar";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/api/", "/panel", "/perfil", "/login", "/register"] },
-    sitemap: "https://www.rosariomarket.com.ar/sitemap.xml",
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/api/",
+        "/admin/",
+        "/profile/",
+        "/panel/",
+        "/mis-productos/",
+        "/ordenes/",
+        "/chatpage/",
+        "/login/",
+        "/register/",
+      ],
+    },
+    sitemap: `${SITE}/sitemap.xml`,
+    host: SITE,
   };
 }
