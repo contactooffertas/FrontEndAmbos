da6008eddd9132c93613ffbfe2a0e914b0f60119
contactooffertas <contacto.offertas@gmail.com>
2026-09-09 17:45:00 -0300

    fix: activar proxy de afiliados antes de cargar paneles

--- app/context/Providers.tsx
+++ app/context/Providers.tsx
@@ -4,10 +4,12 @@ import { AuthProvider } from "./authContext";
 import { CartProvider } from "./cartContext";
 import { TrackingProvider } from "./TrackingContext";
 import GeoPushSync from "../componentes/GeoPushSync";
+import AffiliateFetchProxy from "../componentes/AffiliateFetchProxy";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
+      <AffiliateFetchProxy />
       <GeoPushSync />
       <TrackingProvider>
         <CartProvider>
