e025ae1effb7ba6a8faa503b0c746fab85b31a5e
contactooffertas <contacto.offertas@gmail.com>
2026-09-10 11:29:48 -0300

    Configure Rosario Market Android 2.0 app

---
+++ android-v2/app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rosariomarket.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rosariomarket.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
