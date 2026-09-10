e997bbc399563f3757ef0371536fc8d61f629a65
contactooffertas <contacto.offertas@gmail.com>
2026-09-10 11:55:24 -0300

    feat(web): mount Android download action

--- app/componentes/MainLayout.tsx
+++ app/componentes/MainLayout.tsx
@@ -3,13 +3,14 @@ import Navbar from './navbar';
 import Footer from './footer';
 import SellerOnboardingBanner from './SellerOnboardingBanner';
 import SellerSessionSync from './SellerSessionSync';
-
+import AndroidDownloadButton from './AndroidDownloadButton';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SellerSessionSync />
       <Navbar />
+      <AndroidDownloadButton />
       <main>
         <SellerOnboardingBanner />
         {children}
