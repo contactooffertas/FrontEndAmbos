fa7697aac297bae8d5132ace466c3cb1ce9389a5
contactooffertas <contacto.offertas@gmail.com>
2026-09-12 09:11:17 -0300

    release(android): bump Rosario Market to 3.5.0

--- android-v2/app/build.gradle.kts
+++ android-v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rosariomarket.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 35
+        versionName = "3.5.0"
     }
 
     compileOptions {
